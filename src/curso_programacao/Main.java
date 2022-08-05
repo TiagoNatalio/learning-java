991c391a9f28c42a49e604c620817b4c76ae02f0
TiagoNatalio <tiagonatalio41@gmail.com>
2022-08-04 21:46:02 -0300

    for

--- src/curso_programacao/Main.java
+++ src/curso_programacao/Main.java
@@ -7,15 +7,14 @@ public class Main{
 		
 		Scanner sc = new Scanner(System.in);
 		
-		int x = sc.nextInt();
+		int N = sc.nextInt();
 		
 		int soma = 0;
 		
-		while (x != 0) {
+		for (int i=0; i<N; i++) {
+			int x = sc.nextInt();
 			soma += x;
-			x = sc.nextInt();
 		}
-		
 		System.out.println(soma);
 		
 		sc.close();
